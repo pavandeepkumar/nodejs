071127f3fbc2406cc049777d315b3bfff77ae784
pavandeep.devstree <pavandeep.devstree@gmail.com>
2024-07-28 12:56:43 +0530

    add  route for heatch chekc of server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 
 const app = express()
 
-
-app.listen(3000,()=>{
+app.get('/', (req, res) => {
+    return res.json({ "success": true, "message": "Server is working properly" })
+})
+app.listen(3000, () => {
     console.log("Server listening on port 3000")
 })
